fd0695dabac68a06c3c1742e8afb92cc958bdd70
deutschh <gui.d.andrade10@gmail.com>
2025-08-17 17:29:40 -0300

    atualização na home.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Fluxum from "./assets/fluxum.svg";
 import Relogio from "./assets/clock-3 1.svg";
 import DashBoard from "./assets/box 1 (1).svg";
@@ -7,6 +7,8 @@ import Mapa from "./assets/map 1.svg";
 import Home from "./assets/house 1.svg";
 import Aletrtas from "./assets/flag 1.svg";
 import Relatorio from "./assets/chart-pie 1.svg";
+import Celular from "./assets/CelularH.svg";
+import NotRisco from "./assets/NotRisco.svg";
 
 import { Link } from "react-router-dom";
 
@@ -62,8 +64,17 @@ const App = () => {
     setMenuOpen(!menuOpen);
   };
 
+  // Efeito para adicionar ou remover a classe de rolagem do body
+useEffect(() => {
+  if (menuOpen) {
+    document.body.classList.add('overflow-hidden');
+  } else {
+    document.body.classList.remove('overflow-hidden');
+  }
+}, [menuOpen]);
+
   return (
-    <div className="relative">
+    <div className="relative min-w-full min-h-full">
       {/* Container principal da Nav-bar para desktop */}
       <div className="w-full bg-white md:hidden h-20 flex justify-between items-center">
         {/* Botão do menu para mobile, agora à esquerda */}
@@ -127,7 +138,7 @@ const App = () => {
       {/* Menu para mobile que desliza da esquerda */}
       <div
         className={`
-          md:hidden fixed top-0 left-0 h-screen w-7/12 bg-white border-indigo-400 text-white rounded-e-50 z-50
+          md:hidden fixed top-0 left-0 h-svh overflow-y-auto w-7/12 bg-purple-50 text-white rounded-e-50 z-50
           transform transition-transform duration-300 ease-in-out
           ${menuOpen ? "translate-x-0 " : "-translate-x-full "}
           ${
@@ -137,7 +148,7 @@ const App = () => {
           }
         `}
       >
-        <div className="flex w-5/6 mx-auto justify-center py-6 mt-4 bg-white border-b-2 border-indigo-400">
+        <div className="flex w-5/6 mx-auto justify-center py-6 mt-4 bg-purple-50 border-b-2 border-indigo-400">
           <div className=" text-2xl text-indigo-500 font-bold flex items-center justify-center">
             FLUXUM
           </div>
@@ -153,28 +164,28 @@ const App = () => {
           </a>
           <a
             href="#"
-            className="bg-white py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
+            className="bg-purple-50 py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
           >
             <img src={DashBoard} className="w-1/6 mr-4 ml-3" alt="" />
             Dashboard
           </a>
           <a
             href="#"
-            className="bg-white py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
+            className="bg-purple-50 py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
           >
             <img src={Aletrtas} className="w-1/6 mr-4 ml-3" alt="" />
             Alertas
           </a>
           <a
             href="#"
-            className="bg-white py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
+            className="bg-purple-50 py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
           >
             <img src={Mapa} className="w-1/6 mr-4 ml-3" alt="" />
             Mapas
           </a>
           <a
             href="#"
-            className="bg-white py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
+            className="bg-purple-50 py-3 flex ml-4 w-5/6 text-medium text-indigo-400 font-semibold items-center"
           >
             <img src={Relatorio} className="w-1/6 mr-4 ml-3" alt="" />
             Relatórios
@@ -183,31 +194,48 @@ const App = () => {
         <div className="bg-indigo-500 w-5/6 mx-auto h-12 mt-10 items-center absolute bottom-24 flex justify-center rounded-md text-xl left-1/2 -translate-x-1/2 font-semibold">
           Login
         </div>
-        <div className="bg-white w-5/6 mx-auto h-10 mt-6 items-center bottom-8 absolute flex justify-center left-1/2 -translate-x-1/2 text-indigo-500 rounded-md text-xl font-semibold">
+        <div className="bg-purple-50 w-5/6 mx-auto h-10 mt-6 items-center bottom-8 absolute flex justify-center left-1/2 -translate-x-1/2 text-indigo-500 rounded-md text-xl font-semibold">
           Cadastrar-se
         </div>
       </div>
-
-      <div className="w-11/12 h-96 bg-purple-50 border-stone-100 rounded-2xl mx-auto mt-8 flex relative flex-col shadow-[0px_0px_15px_3px_rgba(25,27,64,0.1)]">
-        <div className="w-5/12 h-8 bg-white text-sm flex justify-center items-center rounded-md text-indigo-500 mx-auto mt-12 shadow-[0px_0px_5px_4px_rgba(25,27,64,0.1)]">
-          Controle o Fluxo
-        </div>
-        <div className="w-11/12 h-22 text-lg justify-center items-center text-center font-bold mx-auto mt-8 text-slate-950">
-          Explore Dados em Fluxo e Monitore o Essencial em Tempo Real.
-        </div>
-        <div className="w-11/12 h-22 text-xs justify-center items-center text-center font-base mx-auto mt-3 text-slate-950">
-          Visualize, analise e tome decisões com base em dados reais, utilizando
-          o sistema inteligente, Fluxum.
-        </div>
-        <div className="flex justify-between w-4/6 mx-auto mt-8">
-          <div className="w-5/12 h-8 bg-indigo-500 text-white flex justify-center items-center rounded-lg">
-            Iniciar
+      <div className="h-[640px] w-full flex flex-col">
+        <div className="w-11/12 h-96 bg-purple-50 border-stone-100 rounded-2xl mx-auto mt-8 flex relative flex-col shadow-[0px_0px_10px_6px_rgba(25,27,64,0.1)]">
+          <div className="w-5/12 h-8 bg-white text-sm flex justify-center items-center rounded-md text-indigo-500 mx-auto mt-12 shadow-[0px_0px_5px_4px_rgba(25,27,64,0.1)]">
+            Controle o Fluxo
           </div>
-          <div className="w-5/12 h-8 bg-white flex justify-center items-center rounded-lg text-indigo-500 shadow-[0px_0px_5px_4px_rgba(25,27,64,0.1)]">
-            Explorar
+          <div className="w-11/12 h-22 text-lg justify-center items-center text-center font-bold mx-auto mt-8 text-slate-950">
+            Explore Dados em Fluxo e Monitore o Essencial em Tempo Real.
+          </div>
+          <div className="w-11/12 h-22 text-xs justify-center items-center text-center font-base mx-auto mt-3 text-slate-950">
+            Visualize, analise e tome decisões com base em dados reais,
+            utilizando o sistema inteligente, Fluxum.
+          </div>
+          <div className="flex justify-between w-4/6 mx-auto mt-8">
+            <div className="w-5/12 h-8 bg-indigo-500 text-white flex justify-center items-center rounded-lg">
+              Iniciar
+            </div>
+            <div className="w-5/12 h-8 bg-white flex justify-center items-center rounded-lg text-indigo-500 shadow-[0px_0px_5px_4px_rgba(25,27,64,0.1)]">
+              Explorar
+            </div>
+          </div>
+          <div className="w-full min-h-72 z-80 bg-indigo-500 rounded-2xl mx-auto mt-8 flex justify-between absolute top-72 border-4 border-indigo-500 left-1/2 -translate-x-1/2">
+            <div className="flex flex-col bg-read-600 w-3/6 min-h-full justify-between">
+            <div className="flex flex-col mt-10 ml-2">
+              <div className="text-xs font-light text-white">
+                O que é o Fluxum?
+              </div>
+              <div className="text-sm mt-2 font-bold text-white">
+                O Sistema Inteligente de Monitoramento Portuário com IoT e
+                Computação em Nuvem
+              </div>
+              </div>
+              <img src={NotRisco} className="ml-2 mb-2" alt="" />
+            </div>
+            <div className="flex flex-col-reverse my-auto w-5/12 mr-2 h-full">
+              <img src={Celular} alt="" className="w-full mx-auto" />
+            </div>
           </div>
         </div>
-        <div className="w-full h-96 z-80 bg-indigo-500 rounded-2xl mx-auto mt-8 flex absolute top-72 border-4 border-indigo-500 flex-col left-1/2 -translate-x-1/2"></div>
       </div>
     </div>
   );
--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    // Adiciona a URL do Ngrok à lista de hosts permitidos
+    host: true, // Isso permite que o servidor seja acessível na rede local
+    allowedHosts: [
+      '057b6b57fb42.ngrok-free.app', // Substitua por sua URL do Ngrok
+    ],
+  },
 })
